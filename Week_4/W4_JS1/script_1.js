a1b8ae6ef9f6a24b41ede8dbcafbe61bed7f53af
PaolaMontaño <paolamo979@gmail.com>
2025-02-20 20:24:15 +0100

    all

--- Week_4/W4_JS1/script_1.js
+++
@@ -1,9 +0,0 @@
-//*Pide al usuario dos números. Muestra el resultado de la resta de ambos, en pantalla y/o en consola*//
-
-let numero1 = prompt("Introduce el primer número:");
-let numero2 = prompt("Introduce el segundo número:");
-
-let resta = numero1 - numero2;
-
-console.log("La resta de ambos números es: " + resta);
-alert("La resta de ambos números es: " + resta);
